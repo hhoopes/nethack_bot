db38fce276847240eaf9d5ff4d6a599284ae6f3f
hhoopes <heidih@gmail.com>
2017-08-22 20:24:15 -0600

    POC of map

--- map_generator.rb
+++ map_generator.rb
@@ -12,39 +12,68 @@ class MapGenerator
     @map = []
     @x = Random.rand(MIN_DIMENSION..MAX_DIMENSION) - 1
     @y = MAX_CHARS/@x - 1
-    @map = Array.new(@y) { Array.new(@x, "") }
+    @map = Array.new(@y) { Array.new(@x, "#") }
   end
 
   def generate_filled_map
-    while filled? == false
-      starting_wall_x, width = Random.rand(0..@x)/2, Random.rand(2..@x)
-      starting_wall_y, height = Random.rand(0..@y), Random.rand(2..@y)
-      byebug
-      width.times do | x_coord |
-        @map[0][starting_wall_x + x_coord] = "|"
+    filled = false
+    while filled == false
+      starting_wall_x = Random.rand(0..@x)/2
+      width = Random.rand(4..(@x - starting_wall_x))
+      starting_wall_y = Random.rand(0..@y)/2
+      height = Random.rand(4..(@y - starting_wall_y))
+      height.times do | y_coord |
+        width.times do | x_coord |
+          assigned_char = assign_map_char(y_max: height, x_max: width, y: y_coord, x: x_coord)
+          @map[starting_wall_y + y_coord][starting_wall_x + x_coord] = assigned_char
+        end
       end
-      byebug
-      puts stringify
+      filled = true
+      puts "Overall map size X: #{@x} Y: #{@y}"
+      puts "displaying room with width of #{width}"
+      puts "height of #{height}"
+      puts "starting x: #{starting_wall_x}, starting wall y: #{starting_wall_y}"
+      add_player
+      stringify
     end
   end
 
   private
-  def stringify
-    map_string = ""
-    @map.each_with_index do | row, index |
-      row.each do |char|
-        if row.count == index
-          map_string << char + '/n'
-        else
-          map_string << char
-        end
-      end
+
+  def add_player
+    map_char = nil
+    player_x = nil
+    player_y = nil
+    until valid_player_locations.include? map_char
+      player_y = Random.rand(0...@y)
+      player_x = Random.rand(0...@x)
+      map_char = @map[player_y][player_x]
     end
-    map_string
+    @map[player_y][player_x] = "@"
+  end
+
+  def valid_player_locations
+    [".", "|", "_"]
+  end
+
+  def assign_map_char(y_max:, x_max:, y:, x:)
+    if y == y_max - 1 || y == 0
+      "-"
+    elsif x == x_max - 1 || x == 0
+      "|"
+    else "."
+    end
+  end
+
+  def stringify
+    @map.each do |r|
+      puts r.each { |char| char }.join('')
+    end
   end
 
   def filled?
-    @map.any? { |char| char == "" }
+    true
+    # @map.any? { |char| char == " " }
   end
   # def [](x, y)
   #   @map[x][y]
