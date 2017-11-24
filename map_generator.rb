873a690074ceebef2e8620de172a5cea3f2635f1
hhoopes <heidih@gmail.com>
2017-11-23 19:59:25 -0700

    Refactor tunnel creation

--- map_generator.rb
+++ map_generator.rb
@@ -9,34 +9,30 @@ class MapGenerator
 
   def initialize(caption = false)
     @caption = caption
-    @map = []
     @x = Random.rand(MIN_DIMENSION..MAX_DIMENSION) - 1
     @y = MAX_CHARS/@x - 1
+    @tunnel_count = Random.rand(6..12)
     @map = Array.new(@y) { Array.new(@x, " ") }
   end
 
   def generate_filled_map
-    filled = false
-    while filled == false
-      starting_wall_x = Random.rand(0..@x)/2
-      width = Random.rand(4..(@x - starting_wall_x))
-      starting_wall_y = Random.rand(0..@y)/2
-      height = Random.rand(4..(@y - starting_wall_y))
-      height.times do | y_coord |
-        width.times do | x_coord |
-          assigned_char = assign_map_char(y_max: height, x_max: width, y: y_coord, x: x_coord)
-          @map[starting_wall_y + y_coord][starting_wall_x + x_coord] = assigned_char
-        end
+    starting_wall_x = Random.rand(0..@x)/2
+    width = Random.rand(4..(@x - starting_wall_x))
+    starting_wall_y = Random.rand(0..@y)/2
+    height = Random.rand(4..(@y - starting_wall_y))
+    height.times do | y_coord |
+      width.times do | x_coord |
+        assigned_char = assign_map_char(y_max: height, x_max: width, y: y_coord, x: x_coord)
+        @map[starting_wall_y + y_coord][starting_wall_x + x_coord] = assigned_char
       end
-      filled = true
-      puts "Overall map size X: #{@x} Y: #{@y}"
-      puts "displaying room with width of #{width}"
-      puts "height of #{height}"
-      puts "starting x: #{starting_wall_x}, starting wall y: #{starting_wall_y}"
-      add_tunnels
-      add_player
-      puts stringify('\n')
     end
+    puts "Overall map size X: #{@x} Y: #{@y}"
+    puts "displaying room with width of #{width}"
+    puts "height of #{height}"
+    puts "starting x: #{starting_wall_x}, starting wall y: #{starting_wall_y}"
+    add_tunnels
+    add_player
+    puts stringify("\n")
   end
 
   private
@@ -53,36 +49,36 @@ class MapGenerator
     @map[player_y][player_x] = "@"
   end
 
-  def add_tunnels
-    random_generate
-  end
-
-  def random_generate(prev_y: nil, prev_x: nil)
+  def add_tunnels(prev_y: nil, prev_x: nil)
     return if enough_tunnels?
     if prev_x
-      if prev_x.even?
+      tunnel_x = 0
+      tunnel_y = 0
+      loop do
         tunnel_y = Random.rand(prev_y - 1..prev_y + 1)
-        tunnel_x = prev_x
-      else
         tunnel_x = Random.rand(prev_x - 1..prev_x + 1)
-        tunnel_y = prev_y
+      break if (tunnel_x + 1 <= @x) && (tunnel_y + 1 <= @y)
       end
     else
-      tunnel_y = Random.rand(0..@y)
-      tunnel_x = Random.rand(0..@x)
+      tunnel_y = Random.rand(0..@y - 1)
+      tunnel_x = Random.rand(0..@x - 1 )
     end
     grab = @map[tunnel_y][tunnel_x]
-    if grab == ' '
+    if grab == ' ' && @map[tunnel_y][tunnel_x]
       @map[tunnel_y][tunnel_x] = '#'
     end
-    random_generate(prev_y: tunnel_y, prev_x: tunnel_x)
+    add_tunnels(prev_y: tunnel_y, prev_x: tunnel_x)
   rescue => e
     # Address assigning a tunnel outside the boundaries
   end
 
   def enough_tunnels?
     tunnel_spaces = stringify.chars.select {|char| char == '#'}.count
-    tunnel_spaces > 6 && tunnel_spaces < 12
+    tunnel_spaces >= @tunnel_count
+  end
+
+  def add_doors
+
   end
 
   def valid_player_locations
