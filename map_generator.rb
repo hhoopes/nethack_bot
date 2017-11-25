059afef5ce715d723cd3f83070ba67f0aee584ef
hhoopes <heidih@gmail.com>
2017-11-25 07:47:15 -0700

    Add traps and refactor constants

--- map_generator.rb
+++ map_generator.rb
@@ -1,21 +1,42 @@
 require 'byebug'
+require 'yaml'
+require 'ostruct'
+require 'json'
 
 class MapGenerator
   MIN_DIMENSION = 14
   MAX_DIMENSION = 20
   MAX_CHARS = 280
-
+  yaml = File.read('lib/map_features.yml')
+  FEATURES = JSON.parse(YAML.load(yaml).to_json, object_class: OpenStruct)
+  # FEATURES = OpenStruct.new(yaml)
   attr_accessor :map
+
 
   def initialize(caption = false)
     @caption = caption
     @x = Random.rand(MIN_DIMENSION..MAX_DIMENSION) - 1
     @y = MAX_CHARS/@x - 1
-    @tunnel_count = Random.rand(6..12)
+    @tunnel_count = Random.rand(FEATURES.structural.tunnel.min..FEATURES.structural.tunnel.max)
+    @door_count = Random.rand(FEATURES.structural.door.min..FEATURES.structural.door.max)
     @map = Array.new(@y) { Array.new(@x, " ") }
+    @flavor = []
+    @locations = {}
   end
 
   def generate_filled_map
+    add_room
+    add_tunnels
+    add_doors
+    add_player
+    add_traps
+    add_monsters
+    puts stringify("\n")
+  end
+
+  private
+
+  def add_room
     starting_wall_x = Random.rand(0..@x)/2
     width = Random.rand(4..(@x - starting_wall_x))
     starting_wall_y = Random.rand(0..@y)/2
@@ -30,23 +51,19 @@ class MapGenerator
     puts "displaying room with width of #{width}"
     puts "height of #{height}"
     puts "starting x: #{starting_wall_x}, starting wall y: #{starting_wall_y}"
-    add_tunnels
-    add_player
-    puts stringify("\n")
   end
-
-  private
 
   def add_player
     map_char = nil
     player_x = nil
     player_y = nil
-    until valid_player_locations.include? map_char
+    until FEATURES.structural.player.char.include? map_char
       player_y = Random.rand(0...@y)
       player_x = Random.rand(0...@x)
       map_char = @map[player_y][player_x]
     end
     @map[player_y][player_x] = "@"
+    @locations[:player] = [player_y, player_x]
   end
 
   def add_tunnels(prev_y: nil, prev_x: nil)
@@ -78,11 +95,60 @@ class MapGenerator
   end
 
   def add_doors
+    @door_count.times do
+      char = ''
+      until char == '|' || char == '_'
+        door_y = Random.rand(0..@y - 1)
+        door_x = Random.rand(0..@x - 1)
+        char = @map[door_y][door_x]
+        if char  == '|' || char == '_'
+          door_char = door_y.even? ? ' ' : '+'
+          @map[door_y][door_x] =  door_char
+        end
+      end
+    end
+  end
+
+  def add_traps
+    trap_count = Random.rand(FEATURES.structural.trap.min..FEATURES.structural.trap.max)
+    trap_count.times do
+      char = ''
+      until char == '.' || char == '#'
+        trap_y = Random.rand(0..@y - 1)
+        trap_x = Random.rand(0..@x - 1)
+        char = @map[trap_y][trap_x]
+        if char  == '.' || char == '#'
+          @map[trap_y][trap_x] = '^'
+          save_flavor_text(:trap) if in_proximity?(:trap, [trap_y, trap_x])
+        end
+      end
+    end
+  end
+
+  def add_monsters
 
   end
 
-  def valid_player_locations
-    ['.', '|', '_', '#']
+  def in_proximity?(type, coordinates)
+    player_y = @locations[:player].first
+    player_x = @locations[:player].last
+    modifier = FEATURES.structural.send(type).proximity
+    diff = (player_y - coordinates.first).abs + (player_x  - coordinates.last).abs
+    puts diff
+    puts "Coordinates: #{coordinates}"
+    puts "y: #{player_y}"
+    puts "x: #{player_x}"
+    puts "diff #{diff}"
+    diff <= modifier
+  rescue => e
+    byebug
+  end
+
+  def save_flavor_text(type)
+    @flavor << FEATURES.structural.send(type).flavor.sample
+    puts @flavor
+  rescue => e
+    byebug
   end
 
   def assign_map_char(y_max:, x_max:, y:, x:)
@@ -100,18 +166,6 @@ class MapGenerator
       acc << string_row
     end
   end
-
-  def filled?
-    true
-    # @map.any? { |char| char == " " }
-  end
-  # def [](x, y)
-  #   @map[x][y]
-  # end
-
-  # def []=(x, y, value)
-  #   @map[x[y]] = value
-  # end
 
 end
 
